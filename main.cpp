e5115ef534f4c8b7b6a4ffac5e22b8b139cdfb46
mocanumatei31 <128747016+mocanumatei31@users.noreply.github.com>
2023-04-05 13:54:00 +0300

    Added Hub Menu and Shop functionality

--- main.cpp
+++ main.cpp
@@ -1,21 +1,75 @@
 #include <iostream>
 
 class Enemy;
-class Weapon;
+
+class Weapon{
+    std::string type;
+    int condition, base_damage;
+    //bool isEquipped;
+public:
+    Weapon(const std::string& type_="Sword",int condition_=100, int base_damage_=150):type(type_),condition(condition_),base_damage(base_damage_){}
+    Weapon(const Weapon& other):type(other.type), condition(other.condition), base_damage(other.base_damage){}
+    friend std::ostream& operator <<(std::ostream& out, const Weapon& weapon)
+    {
+        out<<"Weapon stats:\n";
+        out<<"Weapon Type: "<< weapon.type<<'\n';
+        out<<"Condition: "<< weapon.condition<<"\\100\n";
+        out<<"Base Damage: "<< weapon.base_damage<<"\n";
+        return out;
+    }
+    ~Weapon(){}
+    Weapon& operator=(const Weapon& other)
+    {
+        condition=other.condition;
+        base_damage=other.base_damage;
+        type=other.type;
+        return *this;
+    }
+    bool isFunctional()
+    {
+        return condition>0;
+    }
+    int AttackDamage()
+    {
+        return base_damage - (100 - condition) * base_damage / 100;
+    }
+    void ConditionDecrease(int x)
+    {
+        condition=std::max(0,condition-x);
+    }
+    //Currently unused
+    /**void Equip()
+    {
+        isEquipped=true;
+    }
+    void Unequip()
+    {
+        isEquipped=false;
+    }
+    bool IsWeaponEquipped()
+    {
+        return isEquipped;
+    }*/
+};
+
 
 class Player
 {
+    Weapon weapon;//Player Character's Equipped Weapon
     std::string Name;//Player Character's Name
     int HP, MaxHP, STR, DEF;//Player Character's Attributes
+    int currency=0;//Player Character's Money
 public:
     Player(const std::string& Name_="", int HP_=100, int MaxHP_=100, int STR_=1, int DEF_=1):Name(Name_), HP(HP_), MaxHP(MaxHP_), STR(STR_), DEF(DEF_){}
-    Player(const Player& other):Name(other.Name), HP(other.HP), MaxHP(other.MaxHP), STR(other.STR), DEF(other.DEF){}
+    Player(const Player& other):weapon(other.weapon), Name(other.Name), HP(other.HP), MaxHP(other.MaxHP), STR(other.STR), DEF(other.DEF){}
     friend std::ostream& operator <<(std::ostream& out, const Player& p)
     {
         out<<p.Name<<"'s stats:\n";
         out<<"HP: "<<p.HP<<"/"<<p.MaxHP<<"\n";
         out<<"Strength: "<<p.STR<<"\n";
         out<<"Defence: "<<p.DEF<<"\n";
+        out<<"Your Money: "<<p.currency<<"\n";
+        out<<p.weapon;
         return out;
     }
     friend std::istream& operator >>(std::istream& in, Player& p)
@@ -30,6 +84,7 @@ public:
         MaxHP=other.MaxHP;
         STR=other.STR;
         DEF=other.DEF;
+        weapon=other.weapon;
         return *this;
     }
     ~Player(){}
@@ -42,8 +97,8 @@ public:
         return HP>0;
     }
     void NormalAttack(Enemy& e);
-    void NormalWeaponAttack(Enemy& e, Weapon& weapon);
-    void LightWeaponAttack(Enemy& e, Weapon& weapon);
+    void NormalWeaponAttack(Enemy& e);
+    void LightWeaponAttack(Enemy& e);
     int NormalAttackStrength()
     {
         return 25+25*STR/10;
@@ -51,6 +106,37 @@ public:
     int NormalAttackDefense()
     {
         return 25 * DEF / 10;
+    }
+    void HP_Fill()
+    {
+        HP=MaxHP;
+    }
+    bool isWeaponFunctional()
+    {
+        return weapon.isFunctional();
+    }
+    void WeaponChange(const Weapon &newWeapon)
+    {
+        weapon=newWeapon;
+    }
+    void GainCurrency(int x)
+    {
+        currency+=x;
+    }
+    //Decreases currency and returns 1 if Player has enough currency to make payment
+    //Otherwise returns -1
+    int SpendCurrency(int x)
+    {
+        if(currency-x>=0)
+        {
+            currency-=x;
+            return 1;
+        }
+        else return -1;
+    }
+    void ShowCurrencyAmount()
+    {
+        std::cout<<"Your Money: "<<currency<<"\n";
     }
 };
 
@@ -110,94 +196,52 @@ void Enemy::NormalAttack(Player& p)
     std::cout<<"It has dealt "<<std::max(0,NormalAttackStrength()-p.NormalAttackDefense())<<" Damage\n";
 }
 
-class Weapon{
-    int durability, base_damage;
-    //bool isEquipped;
-public:
-    Weapon(int durability_=100, int base_damage_=150):durability(durability_),base_damage(base_damage_){}
-    friend std::ostream& operator <<(std::ostream& out, const Weapon& weapon)
-    {
-        out<<"Weapon stats:\n";
-        out<<"Durability: "<< weapon.durability<<"\\100\n";
-        out<<"Base Damage: "<< weapon.base_damage<<"\n";
-        return out;
-    }
-    bool isFunctional()
-    {
-       return durability>0;
-    }
-    int AttackDamage()
-    {
-        return base_damage - (100 - durability) * base_damage / 100;
-    }
-    void DurabilityDecrease(int x)
-    {
-        durability=std::max(0,durability-x);
-    }
-    //Currently unused
-    /**void Equip()
-    {
-        isEquipped=true;
-    }
-    void Unequip()
-    {
-        isEquipped=false;
-    }
-    bool IsWeaponEquipped()
-    {
-        return isEquipped;
-    }*/
-};
 
-void Player::NormalWeaponAttack(Enemy& e, Weapon& weapon)
+void Player::NormalWeaponAttack(Enemy& e)
 {
     if(weapon.isFunctional())
     {
         e.HP_Depletion(std::max(0,weapon.AttackDamage()-e.NormalAttackDefense()));
         std::cout<<Name<<" used Weapon Attack\n";
         std::cout<<"It has dealt "<<std::max(0,weapon.AttackDamage()-e.NormalAttackDefense())<<" Damage\n";
-        weapon.DurabilityDecrease(5);
+        weapon.ConditionDecrease(5);
     }
     else
     {
-        std::cout<<"You can't use a broken weapon";
+        std::cout<<"You can't use a broken weapon\n";
     }
 }
 
-void Player::LightWeaponAttack(Enemy& e, Weapon& weapon)
+void Player::LightWeaponAttack(Enemy& e)
 {
     if(weapon.isFunctional())
     {
         e.HP_Depletion(std::max(0,weapon.AttackDamage()/3*2-e.NormalAttackDefense()));
         std::cout<<Name<<" used Light Weapon Attack\n";
         std::cout<<"It has dealt "<<std::max(0,weapon.AttackDamage()/3*2-e.NormalAttackDefense())<<" Damage\n";
-        weapon.DurabilityDecrease(3);
+        weapon.ConditionDecrease(3);
     }
     else
     {
-        std::cout<<"You can't use a broken weapon";
+        std::cout<<"You can't use a broken weapon\n";
     }
 }
 
 class Game
 {
     Player player;
-    Enemy enemy{100, 100, 1, 1};
-    Weapon weapon;
 public:
     Game()=default;
-    Game(const Game& other):player(other.player), enemy(other.enemy), weapon(other.weapon){}
+    Game(const Game& other):player(other.player){}
     ~Game(){}
     Game& operator=(const Game& other)
     {
         player=other.player;
-        enemy=other.enemy;
         return *this;
     }
     friend std::ostream& operator <<(std::ostream& out, const Game& game)
     {
         out<<game.player;
-        out<<game.enemy;
         return out;
     }
     void CharacterCreation()
@@ -207,9 +251,9 @@ public:
         std::cout<<"This is your profile:\n";
         std::cout<<player;
     }
-    //Currently just showcasing fighting mechanics
     void Battle()
     {
+        Enemy enemy{100, 100, 1, 1};
         std::cout<<"This is your opponent:\n";
         std::cout<<enemy<<"\n";
         std::cout<<"Please choose an action:\n";
@@ -233,7 +277,6 @@ public:
             }
             if(op==1) break;
         }
-        std::cout<<weapon;
         bool fled=false;
         while(player.isAlive() && enemy.isAlive())
         {
@@ -253,7 +296,7 @@ public:
                         player.NormalAttack(enemy);
                         break;
                     case 2:
-                        checkWeapon=weapon.isFunctional();
+                        checkWeapon=player.isWeaponFunctional();
                         int attackType;
                         std::cout<<"Choose your Attack Type\n";
                         std::cout<<"1.Light Attack\n";
@@ -262,10 +305,10 @@ public:
                         switch(attackType)
                         {
                             case 1:
-                                player.LightWeaponAttack(enemy,weapon);
+                                player.LightWeaponAttack(enemy);
                                 break;
                             case 2:
-                                player.NormalWeaponAttack(enemy,weapon);
+                                player.NormalWeaponAttack(enemy);
                                 break;
                             default:
                                 invalid=true;
@@ -291,17 +334,121 @@ public:
             enemy.NormalAttack(player);
         }
         if(fled) std::cout<<"You Fled The Battlefield.\n";
-        if(player.isAlive() && !fled) std::cout<<"You Win!\n";
+        if(player.isAlive() && !fled)
+        {
+            std::cout<<"You Win!\n";
+            std::cout<<"You received 50 Coins\n";
+            player.GainCurrency(50);
+        }
         else std::cout<<"You Lose!\n";
     }
-
+    void Shop()
+    {
+        std::cout<<"Welcome to the Shop\n";
+        while(true)
+        {
+            player.ShowCurrencyAmount();
+            std::cout<<"What will you buy?\n";
+            std::cout<<"1.Sword (Price: 250)\n";
+            std::cout<<"2.Spear (Price: 175)\n";
+            std::cout<<"3.Longsword (Price: 350)\n";
+            std::cout<<"4.Dagger(Price: 100)\n";
+            std::cout<<"5.Leave\n";
+            int op;
+            std::cin>>op;
+            int currencyCheck;
+            switch(op)
+            {
+                case 1:
+                {
+                    currencyCheck=player.SpendCurrency(250);
+                    if(currencyCheck==-1)
+                    {
+                        std::cout<<"You don't have enough money to buy this.\n";
+                        break;
+                    }
+                    Weapon w;
+                    player.WeaponChange(w);
+                    std::cout<<"You bought a Sword\n";
+                    break;
+                }
+                case 2:
+                {
+                    currencyCheck = player.SpendCurrency(175);
+                    if (currencyCheck == -1) {
+                        std::cout << "You don't have enough money to buy this.\n";
+                        break;
+                    }
+                    Weapon w{"Spear", 125, 125};
+                    player.WeaponChange(w);
+                    std::cout << "You bought a Spear\n";
+                    break;
+                }
+                case 3:
+                {
+                    currencyCheck = player.SpendCurrency(350);
+                    if (currencyCheck == -1) {
+                        std::cout << "You don't have enough money to buy this.\n";
+                        break;
+                    }
+                    Weapon w{"Longsword", 150, 200};
+                    player.WeaponChange(w);
+                    std::cout << "You bought a Longsword\n";
+                    break;
+                }
+                case 4:
+                {
+                    currencyCheck = player.SpendCurrency(100);
+                    if (currencyCheck == -1) {
+                        std::cout << "You don't have enough money to buy this.\n";
+                        break;
+                    }
+                    Weapon w{"Dagger", 100, 100};
+                    player.WeaponChange(w);
+                    std::cout << "You bought a Dagger\n";
+                    break;
+                }
+                case 5:
+                    return;
+                default:
+                    std::cout<<"Invalid input\n";
+            }
+        }
+    }
+    void HubArea()
+    {
+        while(true)
+        {
+            std::cout<<"Where will you go?\n";
+            std::cout<<"1.Arena\n";
+            std::cout<<"2.Shop\n";
+            std::cout<<"3.Leave\n";
+            int op;
+            std::cin>>op;
+            switch(op)
+            {
+                case 1:
+                    player.HP_Fill();
+                    Battle();
+                    break;
+                case 2:
+                    Shop();
+                    break;
+                case 3:
+                    std::cout<<"Goodbye!\n";
+                    return;
+                default:
+                    std::cout<<"Invalid Input\n";
+                    break;
+            }
+        }
+    }
 };
 
 int main()
 {
     Game game;
     game.CharacterCreation();
-    game.Battle();
+    game.HubArea();
     return 0;
 }
-
