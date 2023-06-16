33f1afda79f4efb516d0c4c42b09d2e65c5aba16
mocanumatei31 <128747016+mocanumatei31@users.noreply.github.com>
2023-06-16 18:27:38 +0300

    CI Fix hopefully

--- weapon.cpp
+++ weapon.cpp
@@ -1,7 +1,5 @@
 #include "weapon.h"
 
-template class Weapon<int>;
-
 Weapon<int> Weapon_Factory::dagger() {return Weapon<int>("Dagger", 100, 100);}
 Weapon<int> Weapon_Factory::spear() {return Weapon<int>("Spear", 125, 125);}
 Weapon<int> Weapon_Factory::sword() {return Weapon<int>("Sword", 100, 150);}
